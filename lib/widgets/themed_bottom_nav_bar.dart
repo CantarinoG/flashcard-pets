3f7eb9ce0979e66e80b35574611ec750e0f61ba7
CantarinoG <guilhermecantarino1709@gmail.com>
2024-08-24 20:31:20 -0300

    feat: Add themed Bottom Navigation Bar.

---
+++ lib/widgets/themed_bottom_nav_bar.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class ThemedBottomNavBar extends StatelessWidget {
+  final int _currentIndex;
+  final Function(int) _onItemTapped;
+  const ThemedBottomNavBar(this._currentIndex, this._onItemTapped, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Color primaryColor = Theme.of(context).colorScheme.primary;
+    Color backgroundColor = Theme.of(context).colorScheme.surface;
+    TextStyle? bodyStyle = Theme.of(context).textTheme.bodySmall;
+
+    return BottomNavigationBar(
+      currentIndex: _currentIndex,
+      onTap: _onItemTapped,
+      showUnselectedLabels: false,
+      selectedItemColor: backgroundColor,
+      unselectedItemColor: backgroundColor,
+      selectedLabelStyle: bodyStyle,
+      unselectedLabelStyle: bodyStyle,
+      items: [
+        BottomNavigationBarItem(
+            icon: const Icon(Icons.home),
+            label: "Principal",
+            backgroundColor: primaryColor),
+        BottomNavigationBarItem(
+            icon: const Icon(Icons.pets),
+            label: "Pets",
+            backgroundColor: primaryColor),
+        BottomNavigationBarItem(
+            icon: const Icon(Icons.diversity_3),
+            label: "Social",
+            backgroundColor: primaryColor),
+        BottomNavigationBarItem(
+            icon: const Icon(Icons.person),
+            label: "Perfil",
+            backgroundColor: primaryColor),
+      ],
+    );
+  }
+}
