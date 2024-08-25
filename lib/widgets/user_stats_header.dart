f336eace91b8c810ee34cb2ef897d3e140d5db08
CantarinoG <guilhermecantarino1709@gmail.com>
2024-08-25 16:49:24 -0300

    feat: Add UserStatsHeader widget.

---
+++ lib/widgets/user_stats_header.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class UserStatsHeader extends StatelessWidget {
+  //Mocked Data
+  final level = 10;
+  final gold = 200;
+  final progress = 0.67;
+
+  const UserStatsHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle? h3 = Theme.of(context).textTheme.headlineSmall;
+    Color primaryColor = Theme.of(context).colorScheme.primary;
+    Color secondaryColor = Theme.of(context).colorScheme.secondary;
+
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Lvl $level",
+              style: h3,
+            ),
+            Row(
+              children: [
+                SvgPicture.asset(
+                  "assets/images/icons/coin.svg",
+                  width: 30,
+                  height: 30,
+                ),
+                Text(
+                  "$gold",
+                  style: h3,
+                )
+              ],
+            ),
+          ],
+        ),
+        Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              gradient: LinearGradient(colors: [
+                primaryColor,
+                secondaryColor,
+                Color.fromARGB(255, 201, 201, 201),
+              ], stops: [
+                progress / 2,
+                progress,
+                progress,
+              ])),
+          child: const SizedBox(height: 8),
+        ),
+      ],
+    );
+  }
+}
