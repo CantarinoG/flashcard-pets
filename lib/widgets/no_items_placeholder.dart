8cfa9f17a7ba7142fb7b53ab95b3be255a011873
CantarinoG <guilhermecantarino1709@gmail.com>
2024-09-02 17:25:34 -0300

    feat: Add NoItemsPlaceholder widget.

---
+++ lib/widgets/no_items_placeholder.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class NoItemsPlaceholder extends StatelessWidget {
+  final String message;
+  const NoItemsPlaceholder(this.message, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle? body = Theme.of(context).textTheme.bodySmall;
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            message,
+            style: body,
+            textAlign: TextAlign.center,
+          ),
+          FractionallySizedBox(
+            widthFactor: 0.7,
+            child: Image.asset('assets/images/no_items_placeholder_image.png'),
+          ),
+        ],
+      ),
+    );
+  }
+}
