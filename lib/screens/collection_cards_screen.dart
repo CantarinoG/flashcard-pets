a811525f4ad0e54aff0591f4d14699d4f2e8bd9a
CantarinoG <guilhermecantarino1709@gmail.com>
2024-09-14 08:41:49 -0300

    feat: Add collection description on top of the screen.

--- lib/screens/collection_cards_screen.dart
+++ lib/screens/collection_cards_screen.dart
@@ -10,10 +10,14 @@ class CollectionCardsScreen extends StatelessWidget {
   //Mocked data
   final String _title = "Cálculo I";
   final List<int> _cards = [1, 1, 1, 1];
+  final String? _description =
+      "Essa coleção é referente à minha disciplina de cálculo I da faculdade.";
   CollectionCardsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final TextStyle? body = Theme.of(context).textTheme.bodySmall;
+
     return Scaffold(
       appBar: ThemedAppBar("$_title (${_cards.length})"),
       floatingActionButton: ThemedFab(() {}, const Icon(Icons.add)),
@@ -25,6 +29,15 @@ class CollectionCardsScreen extends StatelessWidget {
             const SizedBox(
               height: 16,
             ),
+            if (_description != null)
+              Text(
+                _description,
+                style: body,
+              ),
+            if (_description != null)
+              const SizedBox(
+                height: 8,
+              ),
             Expanded(
               child: _cards.isEmpty
                   ? const NoItemsPlaceholder(
