6b554dc0931dac1f8e4b2c0f9876b0b831f8df2d
CantarinoG <guilhermecantarino1709@gmail.com>
2024-09-18 14:36:07 -0300

    feat: Finish StoreScreen UI.

--- lib/screens/store_screen.dart
+++ lib/screens/store_screen.dart
@@ -1,18 +1,34 @@
+import 'package:flashcard_pets/widgets/pet_store_card.dart';
 import 'package:flashcard_pets/widgets/screen_layout.dart';
 import 'package:flashcard_pets/widgets/themed_app_bar.dart';
+import 'package:flashcard_pets/widgets/user_stats_header.dart';
 import 'package:flutter/material.dart';
 
 class StoreScreen extends StatelessWidget {
-  const StoreScreen({super.key});
+  //Mocked data
+  final List<int> _pets = [1, 2, 3];
+  StoreScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: ThemedAppBar("Loja"),
+    return Scaffold(
+      appBar: const ThemedAppBar("Loja"),
       body: ScreenLayout(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [],
+          children: [
+            const UserStatsHeader(),
+            const SizedBox(
+              height: 16,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _pets.length,
+                itemBuilder: (context, index) {
+                  return const PetStoreCard();
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/widgets/pet_store_card.dart
@@ -0,0 +1,81 @@
+import 'package:flashcard_pets/themes/app_themes.dart';
+import 'package:flashcard_pets/widgets/themed_filled_button.dart';
+import 'package:flutter/material.dart';
+
+class PetStoreCard extends StatelessWidget {
+  //Mocked data.
+  final String _imgPath = "assets/images/baby_pets/beagle.png";
+  final String _breed = "Beagle";
+  final bool _knownSkill = true;
+  final String? skillShort = "+2% ouro";
+  const PetStoreCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Color brightColor = Theme.of(context).colorScheme.bright;
+    Color secondary = Theme.of(context).colorScheme.secondary;
+    TextStyle? headLineMedium = Theme.of(context).textTheme.headlineMedium;
+    TextStyle? body = Theme.of(context).textTheme.bodySmall;
+
+    return Card(
+      elevation: 4,
+      color: brightColor,
+      child: Column(
+        children: [
+          AspectRatio(
+            aspectRatio: 2,
+            child: Card(
+              color: brightColor,
+              margin: const EdgeInsets.all(0),
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(12),
+                  topRight: Radius.circular(12),
+                  bottomLeft: Radius.zero,
+                  bottomRight: Radius.zero,
+                ),
+              ),
+              elevation: 4,
+              child: Image.asset(
+                _imgPath,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 4,
+          ),
+          Text(
+            _breed,
+            style: headLineMedium?.copyWith(
+              color: secondary,
+            ),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.star,
+                color: secondary,
+              ),
+              Text(
+                _knownSkill ? " $skillShort" : " Habilidade Desconhecida",
+                style: body,
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          ThemedFilledButton(label: "Comprar", onPressed: () {}),
+          const SizedBox(
+            height: 8,
+          )
+        ],
+      ),
+    );
+  }
+}
