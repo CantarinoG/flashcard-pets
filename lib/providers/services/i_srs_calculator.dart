ae4c93463110bf252f58eeda8e071cfccbffeae5
CantarinoG <guilhermecantarino1709@gmail.com>
2024-09-25 17:31:41 -0300

    feat: Add SM2 algorithm.

---
+++ lib/providers/services/i_srs_calculator.dart
@@ -0,0 +1,6 @@
+import 'package:flashcard_pets/models/flashcard.dart';
+import 'package:flutter/material.dart';
+
+abstract class ISrsCalculator with ChangeNotifier {
+  Flashcard calculateNewValues(Flashcard flashcard, int quality);
+}
---
+++ lib/providers/services/sm2_calculator.dart
@@ -0,0 +1,40 @@
+import 'package:flashcard_pets/models/flashcard.dart';
+import 'package:flashcard_pets/providers/services/i_srs_calculator.dart';
+import 'package:flutter/material.dart';
+
+class Sm2Calculator with ChangeNotifier implements ISrsCalculator {
+  @override
+  Flashcard calculateNewValues(Flashcard flashcard, int quality) {
+    double easinessFactor = flashcard.easeFactor;
+    double interval = flashcard.interval;
+    int repetitions = flashcard.repeticoes;
+
+    if (quality >= 3) {
+      if (repetitions == 0) {
+        interval = 1;
+      } else if (repetitions == 1) {
+        interval = 6;
+      } else {
+        interval = (interval * easinessFactor).round() as double;
+      }
+      repetitions += 1;
+    } else {
+      repetitions = 0;
+      interval = 1;
+    }
+
+    easinessFactor =
+        easinessFactor + (0.1 - (5 - quality) * (0.08 + (5 - quality) * 0.02));
+    if (easinessFactor < 1.3) {
+      easinessFactor = 1.3;
+    }
+
+    flashcard.interval = interval;
+    flashcard.easeFactor = easinessFactor;
+    flashcard.repeticoes = repetitions;
+    flashcard.revisionDate =
+        flashcard.revisionDate.add(Duration(days: interval.floor()));
+
+    return flashcard;
+  }
+}
