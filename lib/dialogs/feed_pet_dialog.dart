89c661da76b217df2823552f937a2fd0f4c9a9d3
CantarinoG <guilhermecantarino1709@gmail.com>
2024-09-27 20:22:25 -0300

    feat: Add user gold to FeedPetDialog.

--- lib/dialogs/feed_pet_dialog.dart
+++ lib/dialogs/feed_pet_dialog.dart
@@ -5,9 +5,12 @@ import 'package:flashcard_pets/providers/services/i_game_elements_calculations.d
 import 'package:flashcard_pets/providers/services/i_json_data_provider.dart';
 import 'package:flashcard_pets/themes/app_text_styles.dart';
 import 'package:flashcard_pets/themes/app_themes.dart';
+import 'package:flashcard_pets/widgets/loading.dart';
+import 'package:flashcard_pets/widgets/no_items_placeholder.dart';
 import 'package:flashcard_pets/widgets/text_field_wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:provider/provider.dart';
 
 class FeedPetDialog extends StatefulWidget {
@@ -56,23 +59,22 @@ class _FeedPetDialog<T> extends State<FeedPetDialog> {
         await Provider.of<IJsonDataProvider<User>>(context, listen: false)
             .readData();
     if (user == null) return;
-    final userGold = user.gold;
-    int? intValue = int.tryParse(value);
-    if (intValue != null && intValue > userGold) {
-      _textController.text = userGold.toString();
-      _textController.selection = TextSelection.fromPosition(
-        TextPosition(offset: _textController.text.length),
-      );
-    }
+
     final userLevel = user.level;
     final gameElementsCalculator =
         Provider.of<IGameElementsCalculations>(context, listen: false);
     final totalXpToMaxLevel =
         gameElementsCalculator.calculateTotalXpToLevel(userLevel);
     final maxXp = totalXpToMaxLevel - widget.pet.totalXp;
+
     final maxCoinsToSpent = (maxXp / xpPerCoin).ceil();
-    if (intValue != null && intValue > maxCoinsToSpent) {
-      _textController.text = maxCoinsToSpent.toString();
+    final userGold = user.gold;
+
+    final maxValue = (maxCoinsToSpent < userGold) ? maxCoinsToSpent : userGold;
+    int? intValue = int.tryParse(value);
+
+    if (intValue != null && intValue > maxValue) {
+      _textController.text = maxValue.toString();
       _textController.selection = TextSelection.fromPosition(
         TextPosition(offset: _textController.text.length),
       );
@@ -84,81 +86,118 @@ class _FeedPetDialog<T> extends State<FeedPetDialog> {
     final TextStyle? h2 = Theme.of(context).textTheme.headlineMedium;
     final TextStyle h4 = Theme.of(context).textTheme.headlineSmallEm;
     final TextStyle bodyEm = Theme.of(context).textTheme.bodySmallEm;
+    final TextStyle? body = Theme.of(context).textTheme.bodySmall;
     final Color secondary = Theme.of(context).colorScheme.secondary;
     final Color primary = Theme.of(context).colorScheme.primary;
     final Color text = Theme.of(context).colorScheme.text;
 
-    return AlertDialog(
-      title: Text(
-        "Alimentar Pet",
-        style: h2?.copyWith(
-          color: secondary,
-        ),
-      ),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          RichText(
-            text: TextSpan(
-              children: [
-                TextSpan(
-                  text: "A cada moeda, o pet é alimentado e ganha ${xpPerCoin}",
-                  style: bodyEm.copyWith(
-                    color: text,
-                  ),
-                ),
-                TextSpan(
-                  text: "XP",
-                  style: h4.copyWith(
-                    color: secondary,
-                  ),
-                ),
-              ],
+    final userProvider =
+        Provider.of<IJsonDataProvider<User>>(context, listen: false);
+
+    return FutureBuilder<User?>(
+      future: userProvider.readData(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Loading();
+        } else if (snapshot.hasError) {
+          return NoItemsPlaceholder('Error: ${snapshot.error}');
+        } else if (!snapshot.hasData) {
+          return const NoItemsPlaceholder('No user data available');
+        }
+
+        final user = snapshot.data!;
+
+        return AlertDialog(
+          title: Text(
+            "Alimentar Pet",
+            style: h2?.copyWith(
+              color: secondary,
             ),
           ),
-          const SizedBox(
-            height: 8,
-          ),
-          TextFieldWrapper(
-            label: "Número de Moedas",
-            child: TextField(
-              controller: _textController,
-              autofocus: true,
-              keyboardType: TextInputType.number,
-              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-              decoration: const InputDecoration(
-                prefixText: "\$",
-                border: InputBorder.none,
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SvgPicture.asset(
+                    "assets/images/custom_icons/coin.svg",
+                    width: 30,
+                    height: 30,
+                  ),
+                  Text(
+                    " ${user.gold}",
+                    style: body,
+                  ),
+                ],
               ),
-              onChanged: _limitValue,
-            ),
-          )
-        ],
-      ),
-      actions: [
-        TextButton(
-          onPressed: () {
-            _cancel(context);
-          },
-          child: Text(
-            "Cancelar",
-            style: bodyEm.copyWith(
-              color: primary,
-            ),
+              const SizedBox(
+                height: 8,
+              ),
+              RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text:
+                          "A cada moeda, o pet é alimentado e ganha ${xpPerCoin}",
+                      style: bodyEm.copyWith(
+                        color: text,
+                      ),
+                    ),
+                    TextSpan(
+                      text: "XP",
+                      style: h4.copyWith(
+                        color: secondary,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              TextFieldWrapper(
+                label: "Número de Moedas",
+                child: TextField(
+                  controller: _textController,
+                  autofocus: true,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  decoration: const InputDecoration(
+                    prefixText: "\$",
+                    border: InputBorder.none,
+                  ),
+                  onChanged: _limitValue,
+                ),
+              )
+            ],
           ),
-        ),
-        TextButton(
-          onPressed: () {
-            _confirm(context);
-          },
-          child: Text(
-            "Alimentar",
-            style: bodyEm.copyWith(
-              color: primary,
+          actions: [
+            TextButton(
+              onPressed: () {
+                _cancel(context);
+              },
+              child: Text(
+                "Cancelar",
+                style: bodyEm.copyWith(
+                  color: primary,
+                ),
+              ),
             ),
-          ),
-        ),
-      ],
+            TextButton(
+              onPressed: () {
+                _confirm(context);
+              },
+              child: Text(
+                "Alimentar",
+                style: bodyEm.copyWith(
+                  color: primary,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
