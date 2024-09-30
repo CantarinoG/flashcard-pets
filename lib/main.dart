2f179bb10b85e4c74c98600eda06d2d2af52442c
CantarinoG <guilhermecantarino1709@gmail.com>
2024-09-30 11:51:54 -0300

    refactor: Make MyAppState public.

--- lib/main.dart
+++ lib/main.dart
@@ -34,13 +34,13 @@ class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<MyApp> createState() => MyAppState();
 
-  static _MyAppState of(BuildContext context) =>
-      context.findAncestorStateOfType<_MyAppState>()!;
+  static MyAppState of(BuildContext context) =>
+      context.findAncestorStateOfType<MyAppState>()!;
 }
 
-class _MyAppState extends State<MyApp> {
+class MyAppState extends State<MyApp> {
   ThemeMode _themeMode = ThemeMode.system;
 
   void changeTheme(ThemeMode themeMode) {
